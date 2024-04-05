bff490daf8c68202a761b6dce9fcb666b41dfa3a
SRINADH <115113319+Srinadhch07@users.noreply.github.com>
2024-04-05 11:55:22 +0530

    Create VowelConsonent.cpp

---
+++ VowelConsonent.cpp
@@ -0,0 +1,26 @@
+//Written by SRINADHCH07
+// Code : checking vowel or consonant
+
+
+
+
+#include<iostream>
+using namespace std;
+
+int main()
+{   char c;
+    cout << "Enter character:\n";
+    cin>>c;
+    if(c=='a'c=='A'c=='E'c=='e'c=='i'c=='I'c=='o'c=='O'c=='u'||c=='U')
+
+    {
+
+        cout<<"Vowel!";
+    }
+    else
+        cout<<"Consonant!";
+    return 0;
+}
+
+
+#c++
