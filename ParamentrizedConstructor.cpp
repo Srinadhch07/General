d4ccdbcc90cb972f4b929bc876c2f70e443e17d9
SRINADH <115113319+Srinadhch07@users.noreply.github.com>
2024-04-05 11:50:31 +0530

    Create ParamentrizedConstructor.cpp

---
+++ ParamentrizedConstructor.cpp
@@ -0,0 +1,21 @@
+//Parametrized constructor
+//written by :SRINADHCH07
+
+#include<iostream>
+#include<cstring>
+using namespace std;
+class s{
+    public :
+int rno;
+char name[20];
+public:
+s(int r, char *y){
+rno=r;
+(name,y);
+}
+};
+int main(){
+s s1(101,"hero");
+cout<<s1.name<<endl<<s1.rno;
+
+#c++
