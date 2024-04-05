d517c566cbc26d7d0a3913630d377f19903b6abe
SRINADH <115113319+Srinadhch07@users.noreply.github.com>
2024-04-05 12:11:56 +0530

    Create Manipulator.cpp

---
+++ Manipulator.cpp
@@ -0,0 +1,13 @@
+//written by SRINADHCH07
+//code: creating manipulator 
+
+#include<iostream>
+#include<iomanip>
+using namespace std;
+ostream & c(ostream &output){
+output<<"Hello srinadh";
+return c;
+}
+cout<<c;
+
+#c++
