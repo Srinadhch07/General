8e58756e411747860cc0088529594c46d36b62ed
SRINADH <115113319+Srinadhch07@users.noreply.github.com>
2025-01-02 23:48:05 +0000

    FOlder created

---
+++ CNS/hill_cipher.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class hill_cipher{
+    String str =" abcdefghijklmnopqrstuvwxyz";
+    char base[] = str.toCharArray();
+
+    public void encryption(String value){
+        String str = "abcd";
+        char input[] = str.toCharArray();
+        int A[][] = new int[2][2];
+        for()
+        
+    }
+
+}
